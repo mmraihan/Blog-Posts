f03df7d0b98ecf2d1efc71317837dc8f053970f7
mmraihan <mm_raihan@live.com>
2021-06-29 14:23:12 +0600

    Adding 5 more components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { PostComponent } from './post/post.component';
+import { PostEditComponent } from './post-edit/post-edit.component';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PostListComponent,
+    PostComponent,
+    PostEditComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule
