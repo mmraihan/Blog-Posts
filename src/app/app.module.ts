ad0c2eeb1ca8e5d76812ae8079250b38db00f9df
mmraihan <mm_raihan@live.com>
2021-06-30 01:40:12 +0600

    Connecting With FormGroup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { PostComponent } from './post/post.component';
 import { PostEditComponent } from './post-edit/post-edit.component';
 import { AuthComponent } from './auth/auth.component';
 import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes=[
   {
@@ -40,7 +41,7 @@ const routes: Routes=[
     AuthComponent
   ],
   imports: [
-    BrowserModule, RouterModule.forRoot(routes)
+    BrowserModule, RouterModule.forRoot(routes), ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/post-edit/post-edit.component.ts
+++ src/app/post-edit/post-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-post-edit',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-edit.component.css']
 })
 export class PostEditComponent implements OnInit {
+  form!: FormGroup;
 
   constructor() { }
 
   ngOnInit(): void {
-  }
+    this.form=new FormGroup({
 
+      title: new FormControl(null),
+      description: new FormControl(null),
+      imagePath: new FormControl(null),
+    });
+  }
+onSubmit(){
+  console.log("Working");
+}
 }
