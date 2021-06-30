b968ebd5871a1a691825aa22bc81d755d821b8e7
mmraihan <mm_raihan@live.com>
2021-06-30 20:33:01 +0600

    Working On saveData

--- src/app/back-end.service.ts
+++ src/app/back-end.service.ts
@@ -1,12 +1,26 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
+import { Post } from './post.model';
+import { PostService } from './post.service';
 
 @Injectable({ providedIn: 'root' })
+export class BackEndService {
 
-export class BackEndService{
-// Fun 1 - Save
+constructor(private postService: PostService){
+
+}
+
+  // Fun 1 - Save
+  savaData() {
+    //Step 1.1- Get list of posts from post.service.ts
+
+    const listOfPosts: Post[]= this.postService.getPosts();
 
 
 
 
-// Fun 2 - Save
+
+    //Step 1.2- Send list of posts to BackEnd
+  }
+
+  // Fun 2 - Save
 }
