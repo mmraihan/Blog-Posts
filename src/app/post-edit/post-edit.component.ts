f7d50bf75f64efad94963f765fc3ab64e8260beb
mmraihan <mm_raihan@live.com>
2021-06-30 12:40:34 +0600

    Adding Post

--- src/app/post-edit/post-edit.component.ts
+++ src/app/post-edit/post-edit.component.ts
@@ -1,26 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Post } from '../post.model';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-post-edit',
   templateUrl: './post-edit.component.html',
-  styleUrls: ['./post-edit.component.css']
+  styleUrls: ['./post-edit.component.css'],
 })
 export class PostEditComponent implements OnInit {
   form!: FormGroup;
 
-  constructor() { }
+  constructor(private postService: PostService) {}
 
   ngOnInit(): void {
-    this.form=new FormGroup({
-
+    this.form = new FormGroup({
       title: new FormControl(null, [Validators.required]),
       description: new FormControl(null, [Validators.required]),
-      imagePath: new FormControl(null,[Validators.required]),
+      imagePath: new FormControl(null, [Validators.required]),
     });
   }
-onSubmit(){
-  console.log("Working");
-  console.log(this.form);
-}
+  onSubmit() {
+    const title = this.form.value.title;
+    const description = this.form.value.description;
+    const imagePath = this.form.value.imagePath;
+
+    //Ready with object
+    const post: Post= new Post(
+      title,
+      description,
+      imagePath,
+      'test@test.com',
+      new Date()
+    );
+
+    //calling service
+    this.postService.addPost(post);
+
+  }
 }
